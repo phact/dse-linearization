328b7ab84f0531cf199914e1de3159331778a972
phact <estevezsebastian@gmail.com>
2018-09-20 12:58:25 -0400

    checkpoint after talking to shooky

--- runebdse.sh
+++ runebdse.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 threads=2
 host=localhost
-./ebdse -vv run type=cql yaml=ledger tags=phase:schema cycles=1 host=$host
-./ebdse -vv run type=cql yaml=ledger tags=phase:table cycles=1 host=$host
-./ebdse -vv run type=cql yaml=ledger tags=phase:write-create-accounts cycles=100000 threads=$threads host=$host
-./ebdse -vv run type=cql yaml=ledger tags=phase:write-lwt cycles=100000 threads=$threads host=$host
+accounts=1
+cycles=100
+type=stdout
+
+#./ebdse -vv run type=$type yaml=ledger tags=phase:schema cycles=1 host=$host
+#./ebdse -vv run type=$type yaml=ledger tags=phase:table cycles=1 host=$host
+#./ebdse -vv run type=$type yaml=ledger tags=phase:write-create-accounts cycles=$cycles threads=$threads host=$host accounts=$accounts
+./ebdse -vvv run type=$type yaml=ledger tags=phase:write-lwt cycles=$cycles threads=$threads host=$host accounts=$accounts
