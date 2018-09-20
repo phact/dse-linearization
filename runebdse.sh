6f93cbfc0f94d6da647003e8332dbd6bad682886
phact <estevezsebastian@gmail.com>
2018-09-19 20:51:32 -0400

    parameterize

--- runebdse.sh
+++ runebdse.sh
@@ -1,2 +1,7 @@
-./ebdse -vv run type=cql yaml=ledger tags=phase:schema cycles=1
-./ebdse -vv run type=cql yaml=ledger tags=phase:table cycles=1
+#!/bin/bash
+threads=2
+host=localhost
+./ebdse -vv run type=cql yaml=ledger tags=phase:schema cycles=1 host=$host
+./ebdse -vv run type=cql yaml=ledger tags=phase:table cycles=1 host=$host
+./ebdse -vv run type=cql yaml=ledger tags=phase:write-create-accounts cycles=100000 threads=$threads host=$host
+./ebdse -vv run type=cql yaml=ledger tags=phase:write-lwt cycles=100000 threads=$threads host=$host
