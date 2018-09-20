063b9f80215a08f98e59a9edab7d56c789ee5ca9
phact <estevezsebastian@gmail.com>
2018-09-20 14:35:16 -0400

    workaround + line 1023 issue

--- runebdse.sh
+++ runebdse.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-threads=2
+threads=1
 host=localhost
 accounts=1
-cycles=100
-type=stdout
+cycles=100000
+type=cql
 
-#./ebdse -vv run type=$type yaml=ledger tags=phase:schema cycles=1 host=$host
-#./ebdse -vv run type=$type yaml=ledger tags=phase:table cycles=1 host=$host
-#./ebdse -vv run type=$type yaml=ledger tags=phase:write-create-accounts cycles=$cycles threads=$threads host=$host accounts=$accounts
+./ebdse -vv run type=$type yaml=ledger tags=phase:schema cycles=1 host=$host
+./ebdse -vv run type=$type yaml=ledger tags=phase:table cycles=1 host=$host
+./ebdse -vv run type=$type yaml=ledger tags=phase:write-create-accounts cycles=$cycles threads=$threads host=$host accounts=$accounts
 ./ebdse -vvv run type=$type yaml=ledger tags=phase:write-lwt cycles=$cycles threads=$threads host=$host accounts=$accounts
