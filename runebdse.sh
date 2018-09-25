e8bc8ac5471b13155c357f805eb1b1c78018e450
phact <estevezsebastian@gmail.com>
2018-09-24 23:44:38 -0400

    schema updates

--- runebdse.sh
+++ runebdse.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-threads=1
+threads=4
 host=localhost
-accounts=1
+accounts=20
 cycles=100000
+retrydelay=1
+maxtries=100
 type=cql
+
 
 ./ebdse -vv run type=$type yaml=ledger tags=phase:schema cycles=1 host=$host
 ./ebdse -vv run type=$type yaml=ledger tags=phase:table cycles=1 host=$host
 ./ebdse -vv run type=$type yaml=ledger tags=phase:write-create-accounts cycles=$cycles threads=$threads host=$host accounts=$accounts
-./ebdse -vvv run type=$type yaml=ledger tags=phase:write-lwt cycles=$cycles threads=$threads host=$host accounts=$accounts
+./ebdse -vv run type=$type yaml=ledger tags=phase:write-lwt errors=retry maxtries=$maxtries cycles=$cycles threads=$threads host=$host accounts=$accounts retrydelay=$retrydelay
