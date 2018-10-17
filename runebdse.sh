27e809b688d734b68ebf7e6253aced0b42f51848
phact <estevezsebastian@gmail.com>
2018-10-17 11:30:26 +0000

    replication factor fix

--- runebdse.sh
+++ runebdse.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+set -x
+
 host=localhost
+REPLICATION=" {'class': 'SimpleStrategy', 'replication_factor': '1'}"
 if [ $# -gt 0 ]; then
     echo $1
     host=$1
 fi
+if [ $# -gt 1 ]; then
+    echo $2
+    REPLICATION=$2
+fi
+
 
 threads=128
 accounts=10
@@ -15,7 +23,7 @@ type=cql
 rate=30
 
 
-/tmp/ebdse/ebdse -vv run type=$type yaml=ledger tags=phase:schema cycles=1 host=$host
+/tmp/ebdse/ebdse -vv run type=$type yaml=ledger tags=phase:schema cycles=1 host=$host REPLICATION="${REPLICATION}"
 /tmp/ebdse/ebdse -vv run type=$type yaml=ledger tags=phase:table cycles=1 host=$host
 /tmp/ebdse/ebdse -vv run type=$type yaml=ledger tags=phase:write-create-accounts cycles=$cycles threads=$threads host=$host accounts=$accounts
 /tmp/ebdse/ebdse -vv run type=$type yaml=ledger tags=phase:write-lwt errors=retry maxtries=$maxtries cycles=$cycles threads=$threads host=$host accounts=$accounts retrydelay=$retrydelay targetrate=$rate
